fa0c01f54ee2db8a295c407d1753a39fc9e7d7f0
sina-xys-felix <sina-xys-felix@users.noreply.github.com>
2025-06-18 06:12:25 +0000

    deploy: fd0031a4885184c987c619361600e769d2215d40

--- assets/avatar.bb589048.js
+++
@@ -1,1 +0,0 @@
-const r=""+new URL("avatar.9ccc7b4b.jpg",import.meta.url).href;export{r as _};
--- assets/table.d548495e.js
+++
@@ -1,1 +0,0 @@
-import{i as r}from"./index.f9b964f5.js";function i(e){return r.get({url:`/list/policy?current=${e.index}&pageSize=${e.size}`})}function s(){return r.get({url:"/sys/list"})}export{s as g,i as q};
