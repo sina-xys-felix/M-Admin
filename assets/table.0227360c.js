65252cf9feabe0865adffbade71d183e026b736f
sina-xys-felix <sina-xys-felix@users.noreply.github.com>
2025-06-18 05:40:34 +0000

    deploy: 44312b94d2c4a370369f806c84579ea6cd62cdec

---
+++ assets/table.0227360c.js
@@ -0,0 +1,1 @@
+import{i as r}from"./index.1f450430.js";function i(e){return r.get({url:`/list/policy?current=${e.index}&pageSize=${e.size}`})}function s(){return r.get({url:"/sys/list"})}export{s as g,i as q};
